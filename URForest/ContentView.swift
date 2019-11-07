903ac5c45142ebf863b0a8346987eff3aa567d71
linweiyan <linweiyan@yy.com>
2019-11-07 10:12:12 +0800

    功能：添加初始文件

---
+++ URForest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URForest
+//
+//  Created by lwy on 2019/11/7.
+//  Copyright © 2019 lwy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
